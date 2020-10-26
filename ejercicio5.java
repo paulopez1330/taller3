b624c64f2e433b8ab01ab106141a2985839b40a4
paulopez1330 <49596062+paulopez1330@users.noreply.github.com>
2020-10-26 20:49:03 -0300

    Create ejercicio5.java

---
+++ ejercicio5.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crud;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+/**
+ *
+ * @author plopez
+ */
+public class DataBase {
+    private static Connection conn;
+    private static final String URL = "jdbc:ucanaccess://C:/prueba/Database11.accdb";
+    
+    public DataBase()
+    {
+        try {
+            if (conn == null) {
+                conn =  DriverManager.getConnection(URL);
+            }
+        } catch (Exception e) {
+            System.out.println("No fue posible conectar la base de datos");
+        }
+    }
+
+    public void Insertar() {
+        Statement stm = null;
+        String query = "Insert Into Productos (id, descripcion, importe) Values (5, 'prueba1', 100.0)";
+        
+        try{
+            stm = conn.createStatement();
+            stm.executeUpdate(query);
+            System.out.println("Se Inserto con éxito");
+        }
+        catch (Exception e){
+            System.out.println("Error al Insertar datos");
+        }        
+    }    
+    
+    public void Eliminar() {
+        Statement stm = null;
+        String query = "Delete From Productos Where ID=1";
+        
+        try{
+            stm = conn.createStatement();
+            stm.executeUpdate(query);
+            System.out.println("Se elimino con éxito");
+        }
+        catch (Exception e){
+            System.out.println("Error al eliminar datos");
+        }        
+    }
+    
+    public void Modificar() {
+        Statement stm = null;
+        String query = "Update Productos SET importe = 100.0 Where ID = 1";
+        
+        try{
+            stm = conn.createStatement();
+            stm.executeUpdate(query);
+            System.out.println("Se actualizó con éxito");
+        }
+        catch (Exception e){
+            System.out.println("Error al modificar datos");
+        }        
+    }
+    
+    public void ImprimiDatos() {
+        Statement stm = null;
+        String query = "Select * from Productos";
+        
+        try {
+            stm = conn.createStatement();
+            ResultSet rs = stm.executeQuery(query);
+            while (rs.next()) {                
+                String producto = rs.getString("descripcion");
+                float precio = rs.getFloat("importe");
+                System.out.println("Producto: " + producto + " IMPORTE: $" + precio);
+            }
+        
+        } catch (Exception e) {
+            System.out.println("No fue posible realizar la consulta a base de datos");
+        }
+    }
+    
+    public void Desconectar() {
+        try {
+            conn.close();
+        } catch (Exception e) {
+            System.out.println("No fue posible cerrar la conexion a la base de datos");
+        }
+    }
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        DataBase d = new DataBase();
+        d.Insertar();
+    }    
+}
